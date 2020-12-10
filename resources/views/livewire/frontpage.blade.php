d29f0c7a69c0ba6d47b59342323734479aad9c6c
Adrian Webman <afnavaja@gmail.com>
2020-12-10 11:03:20 +0800

    Adding Alpine JS for our mobile navigation

--- resources/views/livewire/frontpage.blade.php
+++ resources/views/livewire/frontpage.blade.php
@@ -1,9 +1,10 @@
-<div class="divide-y divide-gray-800">
+<div class="divide-y divide-gray-800" x-data="{ show: false }">
     <nav class="flex items-center bg-gray-900 px-3 py-2 shadow-lg">
         <div>
-            <button class="block h-8 mr-3 text-gray-400 items-center hover:text-gray-200 focus:text-gray-200 focus:outline-none sm:hidden">
+            <button @click="show =! show" class="block h-8 mr-3 text-gray-400 items-center hover:text-gray-200 focus:text-gray-200 focus:outline-none sm:hidden">
                 <svg class="w-8 fill-current" viewBox="0 0 24 24">                            
                     <path x-show="!show" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+                    <path x-show="show" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
                 </svg>
             </button>
         </div>
@@ -37,7 +38,7 @@
             </ul>
 
             {{-- Mobile Web View --}}
-            <div class="pb-3 divide-y divide-gray-800 block sm:hidden">
+            <div :class="show ? 'block' : 'hidden'" class="pb-3 divide-y divide-gray-800 block sm:hidden">
                 <ul class="text-gray-200 text-xs">
                     <a href="{{ url('/home')}}">
                         <li class="cursor-pointer px-4 py-2 hover:bg-gray-800">Home</li>
@@ -59,7 +60,7 @@
             </div>
         </aside>
         <main class="bg-gray-100 p-12 min-h-screen sm:w-8/12 md:w-9/12 lg:w-10/12">
-            <section class="divide-y text-gray-900">
+            <section class="divide-y text-gray-900">    
                 <h1 class="text-3xl font-bold">{{ $title }}</h1>
                 <article>
                     <div class="mt-5 text-sm">                        
